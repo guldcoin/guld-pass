08ddcb0d5262bf72d72003cc4355eca0df5cc786
Ira Miller <public@iramiller.com>
2017-11-29 15:14:37 -0500

    simplified

--- guldpass.py
+++ guldpass.py
@@ -1,7 +1,22 @@
 #! /usr/bin/env python3
 __version__ = '0.0.1'
+import os
 import argparse
-from .index import get_pass, generate_pass
+from random import randint
+from subprocess import check_output
+#from .index import get_pass, generate_pass
+
+try:
+    GEN_PASS_LEN = os.environ['GEN_PASS_LEN']
+except KeyError:
+    # is this pseudo-randomized length actually helping?
+    GEN_PASS_LEN = 25 + randint(-5, 5)
+
+def get_pass(account):
+    return check_output("pass " + account, shell=True).splitlines()[0]
+
+def generate_pass(account):
+    return check_output("pass generate %s %s" % (account, GEN_PASS_LEN), shell=True).splitlines()[-1]
 
 def main():
     parser = argparse.ArgumentParser('guld-pass')
@@ -17,3 +32,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
